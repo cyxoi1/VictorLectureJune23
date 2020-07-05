dd0cb471ba599bb46b35a30645a89bb5b2752041
Nicolae Odagiu <cyxoi1@yandex.ru>
2020-07-05 19:00:21 +0100

    Update loginIhor25.js

--- test/loginIhor25.js
+++ test/loginIhor25.js
@@ -2,9 +2,7 @@ function loginIhor25() {
     browser.deleteAllCookies();
     browser.maximizeWindow();
     browser.url('https://reg.usps.com/entreg/RegistrationAction_input');
-    // $("//input[@name='email']").setValue('lutka@yahoo.com'); // change
-    // $("//input[@name='password']").setValue('lutka'); // change
-    // $("//button[@type='submit']").click();
+    
 }
 
 module.exports = loginIhor25;
